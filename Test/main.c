2f2ae30925ef2ffeb1bed457abb4c06c33842a57
Qryper <simon-schmieden@web.de>
2017-05-01 17:36:32 +0200

    v
    v

--- Test/main.c
+++ Test/main.c
@@ -4,6 +4,15 @@
  *  Created on: 27.04.2017
  *      Author: SimonSchmieden
  */
+#include <stdio.h>
+#include "newHead.h"
+
+int area = CIRCLE_AREA( 4 );
 int main(void){
-	printf("ssss");
+	int x = 5;
+	int y = 10;
+	int z = MAX(x++, y++);
+	printf("%d\n", x);
+	printf("%d\n", y);
+	printf("%d\n", z);
 }
---
+++ Test/newHead.h
@@ -0,0 +1,3 @@
+#define PI 3.14159
+#define CIRCLE_AREA( r ) ( PI * ( r ) * ( r ) )
+#define MAX( a, b ) ((a) < (b) ? (b) : (a))
