2684e4e55ba7a93df355e83fbc01830b6b975b56
Qryper <simon-schmieden@web.de>
2017-05-21 15:23:24 +0200

    Aufg5

---
+++ Aufgabe5/unionstructs.c
@@ -0,0 +1,19 @@
+/*
+ * unionstructs.c
+ *
+ *  Created on: 21.05.2017
+ *      Author: SimonSchmieden
+ */
+
+#import <stdio.h>
+float x;
+
+
+int main(float x){
+	struct floatingP{
+		union {
+
+		};
+	};
+}
+
