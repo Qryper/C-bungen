4a6649e1636d6b4d1c056c16d1127ddb7deb02fa
Qryper <simon-schmieden@web.de>
2017-05-23 15:42:43 +0200

    richtig

--- Aufgabe5/unionstructs.c
+++ Aufgabe5/unionstructs.c
@@ -5,15 +5,41 @@
  *      Author: SimonSchmieden
  */
 
-#import <stdio.h>
-float x;
+#include <stdio.h>
+/*
+typedef union raw_float {		//typedef macht daraus ne Variable /struct muss nicht immer erstellt werden
+	float r_float;
+	struct b_float{				// Typ
+		unsigned s:1;			//falschrum(intuitiv richtig, aber L. Endian)
+		unsigned e:8;
+		unsigned m:23;
+	}bin_float;					//Variable vom Typ kann angelegt werden (Dec/Def einer Variable des Typs)
+}raw_float;
+*/
+
+typedef struct IeeeFloat {
+	unsigned m:23;				//little Endian
+	unsigned e:8;
+	unsigned s:1;
+} IeeeFloat;
+
+union MyFloat {
+	float f;
+	IeeeFloat raw;
+};
+
+typedef union MyFloat MyFloat;
 
 
-int main(float x){
-	struct floatingP{
-		union {
+int main(void){
+	MyFloat f;
+	f.f = -1.0f;				// falsch, da little Endian verwendet wurde (Reihenfolge der Var Decl.)
 
-		};
-	};
+	printf("raw s =%x\n", f.raw.s);
+	printf("raw e =%x\n", f.raw.e);
+	printf("raw m =%x\n", f.raw.m);
+
+
+
 }
 
